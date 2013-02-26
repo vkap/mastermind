b866887f7e4fed339dca03f31d22afa501f261d5
Vitaly Kapustian <vkap@yandex-team.ru>
2013-02-26 14:26:21 +0400

    allow task_id in timed_queue be None (cannot hurry in this case)

--- src/cocaine-app/timed_queue.py
+++ src/cocaine-app/timed_queue.py
@@ -72,11 +72,12 @@ class TimedQueue:
         if self._is_shutting_down():
             return
         with self.__heap_lock:
-            if task_id in self.__task_by_id:
+            if task_id is not None and task_id in self.__task_by_id:
                 raise Exception("Task with ID %s already exists" % task_id)
             task = Task(task_id, function, args, kwargs)
             heapq.heappush(self.__heap, (at, task))
-            self.__task_by_id[task_id] = task
+            if task_id is not None:
+                self.__task_by_id[task_id] = task
     
     def hurry(self, task_id):
         with self.__heap_lock:
